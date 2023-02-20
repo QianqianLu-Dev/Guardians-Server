87daa22d5bf842892b124fa6f57deaf05e48838b
lanyao <xudong0502@foxmail.com>
2023-02-20 14:14:47 +0000

    package

---
+++ target/debug/deps/guardians-ddb4f3c5c0d7c2c1.d
@@ -0,0 +1,5 @@
+/workspace/Guardians-Server/guardians/target/debug/deps/guardians-ddb4f3c5c0d7c2c1.rmeta: src/main.rs
+
+/workspace/Guardians-Server/guardians/target/debug/deps/guardians-ddb4f3c5c0d7c2c1.d: src/main.rs
+
+src/main.rs:
